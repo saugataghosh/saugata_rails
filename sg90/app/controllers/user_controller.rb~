5e258693f26d2ddb0687edc6b3eea68a2aea9767
saugataghosh <saugataghosh90@gmail.com>
2012-07-16 00:14:56 +0530

    rails

---
+++ sg90/app/controllers/user_controller.rb~
@@ -0,0 +1,9 @@
+class UserController < ApplicationController
+def index
+@users=User.all
+end
+def show
+@users=User.find(param[:id])
+end
+end
+
---
+++ sg90/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sg90::Application.config.secret_token = '4267a6b6747b59a30dbe54a3fbd4d34c69e690a02ba46c1b30dccf5d68166de371613d10c598ec8faa98656c200924734e9c20ac7a08a88efbd8cb42836ede1c'
